4429d744088a6f591c2cc84620b2e7636a544ef8
Pendan <daniele.scarinci@studenti.unicam.it>
2015-03-08 03:44:00 +0100

    update protection

--- integrali.f90
+++ integrali.f90
@@ -6,6 +6,7 @@ REAL*8 :: SUMMA=0.D0
 INTEGER :: I=0,CON=0,SCELTA=0
 
 1 call system('cls')
+CON=0
 SUMMA=0.D0
 ERRORE=0.D0
 SUMMA1=0.D0
@@ -13,17 +14,17 @@ CASU=0.D0
 WRITE (*,*) "Mono-Integral Calculator v0.01, di Daniele Scarinci"
 WRITE (*,*)
 WRITE (*,"(A)") "Inserire: 1 = Versione grafica 2 = Versione normale 3 = Versione veloce"
-WRITE (*,"(A)") "Premere qualsiasi altro tasto per uscire."
+WRITE (*,"(A)") "Premere 4 per uscire."
 
-READ (*,*) SCELTA
+10 READ (*,*,ERR=10) SCELTA
 
 
 IF (SCELTA.EQ.1) THEN
-
+CON=0
 WRITE (*,"(A)",ADVANCE="NO") "Inserire numero di iterazioni:"
-
-READ (*,*) CON
-
+DO WHILE(CON.LE.0)
+11 READ (*,*,ERR=11) CON
+END DO
 
 
 DO I=1,CON
@@ -68,11 +69,11 @@ SUMMA=SUMMA/CON
 
 WRITE (*,*) 
 WRITE (*,*) 
-WRITE (*,*) 
-WRITE (*,*) "Valore reale:", PI4
-WRITE (*,*) "Valore trovato:", SUMMA
-WRITE (*,*) "Errore:", ABS(PI4-SUMMA)
-WRITE (*,*) "Errore Percentuale:", ABS(ERRORE),"%"
+WRITE (*,*) "Iterazioni: ", CON
+WRITE (*,*) "Valore reale: ", PI4
+WRITE (*,*) "Valore trovato: ", SUMMA
+WRITE (*,*) "Errore :", ABS(PI4-SUMMA)
+WRITE (*,*) "Errore Percentuale: ", ABS(ERRORE),"%"
 WRITE (*,*)
 WRITE (*,*) "Premere Invio per continuare"
 READ (*,*)
@@ -80,12 +81,12 @@ GOTO 1
 
 
 ELSE IF (SCELTA.EQ.2) THEN
-
+CON=0
 WRITE (*,"(A)",ADVANCE="NO") "Inserire numero di iterazioni:"
 
-
-READ (*,*) CON
-
+DO WHILE(CON.LE.0)
+12 READ (*,*, ERR=12) CON
+END DO
 
 
 DO I=1,CON
@@ -102,23 +103,23 @@ SUMMA=SUMMA/CON
 
 WRITE (*,*) 
 WRITE (*,*) 
-WRITE (*,*) 
-WRITE (*,*) "Valore reale:", PI4
-WRITE (*,*) "Valore trovato:", SUMMA
-WRITE (*,*) "Errore:", ABS(PI4-SUMMA)
-WRITE (*,*) "Errore Percentuale:",ABS(ERRORE),"%"
+WRITE (*,*) "Iterazioni: ", CON
+WRITE (*,*) "Valore reale: ", PI4
+WRITE (*,*) "Valore trovato: ", SUMMA
+WRITE (*,*) "Errore: ", ABS(PI4-SUMMA)
+WRITE (*,*) "Errore Percentuale: ",ABS(ERRORE),"%"
 WRITE (*,*)
 WRITE (*,*) "Premere Invio per continuare"
 READ (*,*)
 GOTO 1
 
 ELSE IF (SCELTA.EQ.3) THEN
-
+CON=0
 WRITE (*,"(A)",ADVANCE="NO") "Inserire numero di iterazioni:"
 
-
-READ (*,*) CON
-
+DO WHILE(CON.LE.0)
+13 READ (*,*,ERR=13) CON
+END DO
 
 
 DO I=1,CON
@@ -133,19 +134,23 @@ SUMMA=SUMMA/CON
 
 WRITE (*,*) 
 WRITE (*,*) 
-WRITE (*,*) 
-WRITE (*,*) "Valore reale:", PI4
-WRITE (*,*) "Valore trovato:", SUMMA
-WRITE (*,*) "Errore:", ABS(PI4-SUMMA)
-WRITE (*,*) "Errore Percentuale:", ABS(((PI4-SUMMA)/PI4)*100),"%"
+WRITE (*,*) "Iterazioni: ", CON
+WRITE (*,*) "Valore reale: ", PI4
+WRITE (*,*) "Valore trovato: ", SUMMA
+WRITE (*,*) "Errore: ", ABS(PI4-SUMMA)
+WRITE (*,*) "Errore Percentuale: ", ABS(((PI4-SUMMA)/PI4)*100),"%"
 WRITE (*,*)
 WRITE (*,*) "Premere Invio per continuare"
 READ (*,*)
 GOTO 1
 
+ELSE IF (SCELTA.EQ.4) THEN
+CON=0
+GOTO 2
+
 ELSE
 
-GOTO 2
+GOTO 10
 
 END IF
 
